1e99a0b6159829a4531d1ff754425707121c9b0b
Alexander Freeman <alexander.freeman.nl@gmail.com>
2022-05-29 19:10:14 +0200

    Add columns and inserting for features

--- data_generation/multicore.sh
+++ data_generation/multicore.sh
@@ -3,5 +3,5 @@
 for VARIABLE in {1..15}
 do
   echo $VARIABLE
-  taskset -c $VARIABLE python main.py &
+  taskset -c $VARIABLE python run_solver.py &
 done
--- data_generation/run_solver.py
+++ data_generation/run_solver.py
@@ -33,6 +33,7 @@ def run_problem(model: path, problem_instance: path, executable: path = 'minizin
     limit (TL) and saves all the output features at certain `save_points` percentages
     of that TL.
     """
+
     # error checking
     if run(['which', executable], stdout=DEVNULL).returncode != 0:
         raise FileNotFoundError(f'Executable ({executable}) was not found')
@@ -70,7 +71,6 @@ def run_problem(model: path, problem_instance: path, executable: path = 'minizin
                 if is_stat_value(line):  # new value to save
                     name, stat = line[13:].split('=')
                     output.features[name] = float(stat)
-                    print(name, output.features[name])
 
                 elif stat_block_end(line):  # end of stat block to capture
                     capture_next_block = False
@@ -123,28 +123,334 @@ def read_next_problem_from_db(db_path):
     db = sqlite3.connect(db_path)
     cursor = db.cursor()
     cursor.execute("SELECT id, mzn, dzn from todo limit 1;")
-    values = cursor.fetchone()
+    values = cursor.fetchall()
     db.close()
     if len(values) == 0:
         return (None, None, None)
     return values[0]
 
-def insert_result_set_in_db(db_path, result_set):
+def insert_result_set_in_db(db_path, mzn, dzn, result_set):
     """
     Inserts a `result_set` into a sqlite3 db at `db_path`.
     """
+    db = sqlite3.connect(db_path)
+    cursor = db.cursor()
+
+    result_set.results()
+    cursor.execute("""
+        INSERT INTO features(
+            mzn,
+            dzn,
+
+            p5_conflicts,
+            p5_ewma_conflicts,
+            p5_nodes,
+            p5_ewma_opennodes,
+            p5_vars,
+            p5_back_jumps,
+            p5_ewma_back_jumps,
+            p5_solutions,
+            p5_total_time,
+            p5_search_time,
+            p5_intVars,
+            p5_propagations,
+            p5_ewma_propagations,
+            p5_propagators,
+            p5_boolVars,
+            p5_learnt,
+            p5_bin,
+            p5_tern,
+            p5_long_vars,
+            p5_peak_depth,
+            p5_best_objective,
+            p5_ewma_best_objective,
+
+            p10_conflicts,
+            p10_ewma_conflicts,
+            p10_nodes,
+            p10_ewma_opennodes,
+            p10_vars,
+            p10_back_jumps,
+            p10_ewma_back_jumps,
+            p10_solutions,
+            p10_total_time,
+            p10_search_time,
+            p10_intVars,
+            p10_propagations,
+            p10_ewma_propagations,
+            p10_propagators,
+            p10_boolVars,
+            p10_learnt,
+            p10_bin,
+            p10_tern,
+            p10_long_vars,
+            p10_peak_depth,
+            p10_best_objective,
+            p10_ewma_best_objective,
+
+            p15_conflicts,
+            p15_ewma_conflicts,
+            p15_nodes,
+            p15_ewma_opennodes,
+            p15_vars,
+            p15_back_jumps,
+            p15_ewma_back_jumps,
+            p15_solutions,
+            p15_total_time,
+            p15_search_time,
+            p15_intVars,
+            p15_propagations,
+            p15_ewma_propagations,
+            p15_propagators,
+            p15_boolVars,
+            p15_learnt,
+            p15_bin,
+            p15_tern,
+            p15_long_vars,
+            p15_peak_depth,
+            p15_best_objective,
+            p15_ewma_best_objective,
+
+            p20_conflicts,
+            p20_ewma_conflicts,
+            p20_nodes,
+            p20_ewma_opennodes,
+            p20_vars,
+            p20_back_jumps,
+            p20_ewma_back_jumps,
+            p20_solutions,
+            p20_total_time,
+            p20_search_time,
+            p20_intVars,
+            p20_propagations,
+            p20_ewma_propagations,
+            p20_propagators,
+            p20_boolVars,
+            p20_learnt,
+            p20_bin,
+            p20_tern,
+            p20_long_vars,
+            p20_peak_depth,
+            p20_best_objective,
+            p20_ewma_best_objective,
+        ) VALUES(
+            ?, ?, ?, ?, ?,, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+        ) """
+        , (
+            mzn,
+            dzn,
+
+            result_set[0].features['conflicts'],
+            result_set[0].features['ewma_conflicts'],
+            result_set[0].features['nodes'],
+            result_set[0].features['ewma_opennodes'],
+            result_set[0].features['vars'],
+            result_set[0].features['back_jumps'],
+            result_set[0].features['conflicts'],
+            result_set[0].features['ewma_back_jumps'],
+            result_set[0].features['solutions'],
+            result_set[0].features['total_time'],
+            result_set[0].features['search_time'],
+            result_set[0].features['intVars'],
+            result_set[0].features['propagations'],
+            result_set[0].features['ewma_propagations'],
+            result_set[0].features['propagators'],
+            result_set[0].features['boolVars'],
+            result_set[0].features['learnt'],
+            result_set[0].features['bin'],
+            result_set[0].features['tern'],
+            result_set[0].features['long'],
+            result_set[0].features['peak_depth'],
+            result_set[0].features['best_objective'],
+            result_set[0].features['ewma_best_objective'],
+
+            result_set[1].features['conflicts'],
+            result_set[1].features['ewma_conflicts'],
+            result_set[1].features['nodes'],
+            result_set[1].features['ewma_opennodes'],
+            result_set[1].features['vars'],
+            result_set[1].features['back_jumps'],
+            result_set[1].features['conflicts'],
+            result_set[1].features['ewma_back_jumps'],
+            result_set[1].features['solutions'],
+            result_set[1].features['total_time'],
+            result_set[1].features['search_time'],
+            result_set[1].features['intVars'],
+            result_set[1].features['propagations'],
+            result_set[1].features['ewma_propagations'],
+            result_set[1].features['propagators'],
+            result_set[1].features['boolVars'],
+            result_set[1].features['learnt'],
+            result_set[1].features['bin'],
+            result_set[1].features['tern'],
+            result_set[1].features['long'],
+            result_set[1].features['peak_depth'],
+            result_set[1].features['best_objective'],
+            result_set[1].features['ewma_best_objective'],
+
+            result_set[2].features['conflicts'],
+            result_set[2].features['ewma_conflicts'],
+            result_set[2].features['nodes'],
+            result_set[2].features['ewma_opennodes'],
+            result_set[2].features['vars'],
+            result_set[2].features['back_jumps'],
+            result_set[2].features['conflicts'],
+            result_set[2].features['ewma_back_jumps'],
+            result_set[2].features['solutions'],
+            result_set[2].features['total_time'],
+            result_set[2].features['search_time'],
+            result_set[2].features['intVars'],
+            result_set[2].features['propagations'],
+            result_set[2].features['ewma_propagations'],
+            result_set[2].features['propagators'],
+            result_set[2].features['boolVars'],
+            result_set[2].features['learnt'],
+            result_set[2].features['bin'],
+            result_set[2].features['tern'],
+            result_set[2].features['long'],
+            result_set[2].features['peak_depth'],
+            result_set[2].features['best_objective'],
+            result_set[2].features['ewma_best_objective'],
+
+            result_set[3].features['conflicts'],
+            result_set[3].features['ewma_conflicts'],
+            result_set[3].features['nodes'],
+            result_set[3].features['ewma_opennodes'],
+            result_set[3].features['vars'],
+            result_set[3].features['back_jumps'],
+            result_set[3].features['conflicts'],
+            result_set[3].features['ewma_back_jumps'],
+            result_set[3].features['solutions'],
+            result_set[3].features['total_time'],
+            result_set[3].features['search_time'],
+            result_set[3].features['intVars'],
+            result_set[3].features['propagations'],
+            result_set[3].features['ewma_propagations'],
+            result_set[3].features['propagators'],
+            result_set[3].features['boolVars'],
+            result_set[3].features['learnt'],
+            result_set[3].features['bin'],
+            result_set[3].features['tern'],
+            result_set[3].features['long'],
+            result_set[3].features['peak_depth'],
+            result_set[3].features['best_objective'],
+            result_set[3].features['ewma_best_objective'],
+            problem_results.solved
+        )
+    )
+
+    db.commit()
+    db.close()
+
+
 def setup_db(db_path):
     """
     Inserts a feature table into a sqlite3 db at `db_path`
     """
     db = sqlite3.connect(db_path)
     cursor = db.cursor()
-    feature_table = """
-    CREATE TABLE IF NOT EXISTS features (
+    feature_table = """ CREATE TABLE if not exists features(
             mzn VARCHAR(255) not null,
             dzn VARCHAR(255) not null,
+
+            p5_conflicts INTEGER not null,
+            p5_ewma_conflicts INTEGER not null,
+            p5_nodes INTEGER not null,
+            p5_ewma_opennodes INTEGER not null,
+            p5_vars INTEGER not null,
+            p5_back_jumps INTEGER not null,
+            p5_ewma_back_jumps INTEGER not null,
+            p5_solutions INTEGER not null,
+            p5_total_time REAL not null,
+            p5_search_time REAL not null,
+            p5_intVars INTEGER not null,
+            p5_propagations INTEGER not null,
+            p5_ewma_propagations INTEGER not null,
+            p5_propagators INTEGER not null,
+            p5_boolVars INTEGER not null,
+            p5_learnt INTEGER not null,
+            p5_bin INTEGER not null,
+            p5_tern INTEGER not null,
+            p5_long_vars INTEGER not null,
+            p5_peak_depth INTEGER not null,
+            p5_best_objective INTEGER,
+            p5_ewma_best_objective INTEGER,
+            p5_time_limit_reached BOOLEAN not null,
+
+            p10_conflicts INTEGER not null,
+            p10_ewma_conflicts INTEGER not null,
+            p10_nodes INTEGER not null,
+            p10_ewma_opennodes INTEGER not null,
+            p10_vars INTEGER not null,
+            p10_back_jumps INTEGER not null,
+            p10_ewma_back_jumps INTEGER not null,
+            p10_solutions INTEGER not null,
+            p10_total_time REAL not null,
+            p10_search_time REAL not null,
+            p10_intVars INTEGER not null,
+            p10_propagations INTEGER not null,
+            p10_ewma_propagations INTEGER not null,
+            p10_propagators INTEGER not null,
+            p10_boolVars INTEGER not null,
+            p10_learnt INTEGER not null,
+            p10_bin INTEGER not null,
+            p10_tern INTEGER not null,
+            p10_long_vars INTEGER not null,
+            p10_peak_depth INTEGER not null,
+            p10_best_objective INTEGER,
+            p10_ewma_best_objective INTEGER,
+            p10_time_limit_reached BOOLEAN not null,
+
+            p15_conflicts INTEGER not null,
+            p15_ewma_conflicts INTEGER not null,
+            p15_nodes INTEGER not null,
+            p15_ewma_opennodes INTEGER not null,
+            p15_vars INTEGER not null,
+            p15_back_jumps INTEGER not null,
+            p15_ewma_back_jumps INTEGER not null,
+            p15_solutions INTEGER not null,
+            p15_total_time REAL not null,
+            p15_search_time REAL not null,
+            p15_intVars INTEGER not null,
+            p15_propagations INTEGER not null,
+            p15_ewma_propagations INTEGER not null,
+            p15_propagators INTEGER not null,
+            p15_boolVars INTEGER not null,
+            p15_learnt INTEGER not null,
+            p15_bin INTEGER not null,
+            p15_tern INTEGER not null,
+            p15_long_vars INTEGER not null,
+            p15_peak_depth INTEGER not null,
+            p15_best_objective INTEGER,
+            p15_ewma_best_objective INTEGER,
+            p15_time_limit_reached BOOLEAN not null,
+
+            p20_conflicts INTEGER not null,
+            p20_ewma_conflicts INTEGER not null,
+            p20_nodes INTEGER not null,
+            p20_ewma_opennodes INTEGER not null,
+            p20_vars INTEGER not null,
+            p20_back_jumps INTEGER not null,
+            p20_ewma_back_jumps INTEGER not null,
+            p20_solutions INTEGER not null,
+            p20_total_time REAL not null,
+            p20_search_time REAL not null,
+            p20_intVars INTEGER not null,
+            p20_propagations INTEGER not null,
+            p20_ewma_propagations INTEGER not null,
+            p20_propagators INTEGER not null,
+            p20_boolVars INTEGER not null,
+            p20_learnt INTEGER not null,
+            p20_bin INTEGER not null,
+            p20_tern INTEGER not null,
+            p20_long_vars INTEGER not null,
+            p20_peak_depth INTEGER not null,
+            p20_best_objective INTEGER,
+            p20_ewma_best_objective INTEGER,
+            p20_time_limit_reached BOOLEAN not null,
             PRIMARY KEY(mzn, dzn)
     ); """
+    cursor.execute(feature_table)
     db.commit()
     db.close()
 
@@ -160,7 +466,16 @@ if __name__ == '__main__':
     setup_db(db_path)
 
     id, mzn, dzn = read_next_problem_from_db(db_path)
+    remove_id_from_todo_list(db_path, id)
+
+    save_points = [0.05, 0.1, 0.15, 0.2]
+
     while mzn != None:
-        result_set = run_problem(mzn, dzn, save_points=[0, 0.05, 0.1, 0.15, 0.2])
-        insert_result_set_in_db(db_path, mzn, dzn, result_set)
+        if dzn is not None:
+            result_set = run_problem(mzn, dzn, save_points=save_points)
+
+            # Otherwise we don't reach 20%!
+            if len(result_set.results) == len(save_points):
+                insert_result_set_in_db(db_path, mzn, dzn, result_set)
+        id, mzn, dzn = read_next_problem_from_db(db_path)
         remove_id_from_todo_list(db_path, id)
