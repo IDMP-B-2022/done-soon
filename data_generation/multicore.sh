158a7eeb628dc10dc2f1ebbe8f8ab8cac984e747
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2022-10-13 13:14:11 +0200

    add solver mode to multicore bash script

--- data_generation/multicore.sh
+++ data_generation/multicore.sh
@@ -1,8 +1,9 @@
-# Does stuff
+# Runs a number of solver instances
+# Takes as an argument, the mode to run the solver in. Either `label` or `features`
 
 for VARIABLE in {0..8}
 do
   echo $VARIABLE
-  taskset -c $VARIABLE python3 run_solver.py &
+  taskset -c $VARIABLE python3 run_solver.py --mode $1 &
   sleep 1
 done
