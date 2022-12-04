d644d034ee7b563b7ed7db800a54544565f4f323
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2022-12-04 18:52:20 +0100

    add generated lists of problem sources

---
+++ resources/download/get_archive_links.py
@@ -0,0 +1,45 @@
+import argparse
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+import requests
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", help="Output directory", type=Path, required=True)
+    args = parser.parse_args()
+
+    for source in ["miplib", "satlib", "minizinc_challenges", "minizinc_benchmark"]:
+        list_of_links = []
+        if source == "satlib":
+            base_url = 'https://www.cs.ubc.ca/~hoos/SATLIB/'
+            res = requests.get(base_url + '/benchm.html', timeout=5)
+            soup = BeautifulSoup(res.text, 'html.parser')
+
+            list_of_links = [
+                base_url[8:] + link.get('href')
+                for link in soup.find_all('a')
+                if link.get('href')[-7:] == '.tar.gz'
+            ]
+        if source == "miplib":
+            list_of_links = ['drive.google.com/uc?id=1n0RZLUdFBW4Nhmdwj02VqW_gG6s8S8I8&confirm=t']
+        if source == "minizinc_benchmarks":
+            list_of_links = ['github.com/MiniZinc/minizinc-benchmarks/archive/refs/heads/master.zip']
+        if source == "minizinc_challenges":
+            list_of_links = [
+                f"www.minizinc.org/challenge{year}/mznc{year}-probs.tar.gz"
+                for year in range(2013, 2021)
+            ] + [
+                f"www.minizinc.org/challenge{year}/mznc{year}_probs.tar.gz"
+                for year in range(2021, 2023)
+            ]
+        if not args.output.exists():
+            args.output.mkdir()
+
+        with open(f"{args.output}/links-{source}.json", "w") as out_file:
+            out_file.writelines(list_of_links)
+
+
+if __name__ == "__main__":
+    main()
