4b1bb11258d72e20b3b1d02e459ac7573978a785
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2023-01-16 02:21:51 +0100

    create problem output path if doesn't exist

--- workflow/scripts/ingest_bson.py
+++ workflow/scripts/ingest_bson.py
@@ -2,17 +2,22 @@ import argparse
 import json
 import sys
 import bson
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument("outfile")
+parser.add_argument("outfile_path", type=Path)
 args = parser.parse_args()
 
-with open(args.outfile, 'wb') as outfile:
-    for line in sys.stdin:
-        try:
-            load = json.loads(line)
-            outfile.write(bson.dumps(load))
-        except json.decoder.JSONDecodeError as e:
-            print(f"JSON Encoding error: {e}, {line}", file=sys.stderr)
-        except bson.codec.UnknownSerializerError as e:
-            print(f"BSON Encoding error: {e}, {line}", file=sys.stderr)
+outfile_path = Path(args.outfile_path)
+outfile_path.parent.mkdir(exist_ok=True, parents=True)
+
+with open(outfile_path, 'wb') as outfile:
+    for i, line in enumerate(sys.stdin):
+        if i % 100 == 0:
+            try:
+                load = json.loads(line)
+                outfile.write(bson.dumps(load))
+            except json.decoder.JSONDecodeError as e:
+                print(f"JSON Encoding error: {e}, {line}", file=sys.stderr)
+            except bson.codec.UnknownSerializerError as e:
+                print(f"BSON Encoding error: {e}, {line}", file=sys.stderr)
