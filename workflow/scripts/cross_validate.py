c5306a8ffc146f3fae96c17264d2fa59170ca6e1
Alexander Freeman <alexander.freeman.nl@gmail.com>
2023-06-03 12:48:41 +0200

    Add bare bones cross validate script

--- workflow/scripts/cross_validate.py
+++ workflow/scripts/cross_validate.py
@@ -1,18 +1,22 @@
 import json
 import logging
 import pickle
-import sys
 from argparse import ArgumentParser
-from glob import glob
 from pathlib import Path
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
+
+import pandas as pd
+import numpy as np
 from sklearn.dummy import DummyClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 logging.basicConfig(
     level="NOTSET", format="%(message)s", datefmt="[%X]"
@@ -22,19 +26,74 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def create_models():
-    models = {'LR': LogisticRegression(max_iter=1000, C=1000, class_weight='balanced', random_state=22),
-              'SVM': SVC(kernel='rbf', class_weight='balanced', probability=True, random_state=22),
-              'RF': RandomForestClassifier(min_samples_leaf=5, class_weight='balanced_subsample', random_state=22),
-              'ET': ExtraTreesClassifier(class_weight='balanced', random_state=22),
-              'MLP': MLPClassifier(random_state=22), 'AB': AdaBoostClassifier(),
-              'DT': DecisionTreeClassifier(max_depth=5, class_weight='balanced', random_state=22),
+def create_model(model):
+    models = {'LR': LogisticRegression(max_iter=1000, C=1000, class_weight='balanced'),
+              'SVM': SVC(kernel='rbf', class_weight='balanced', probability=True),
+              'RF': RandomForestClassifier(min_samples_leaf=5, class_weight='balanced_subsample'),
+              'ET': ExtraTreesClassifier(class_weight='balanced'),
+              'MLP': MLPClassifier(),
+              'AdaBoost': AdaBoostClassifier(),
+              'DT': DecisionTreeClassifier(max_depth=5, class_weight='balanced'),
               'DUM': DummyClassifier(strategy="stratified")}
-    return models
+    return models[model]
+
+
+def preprocessing(dataframe, target):
+    result = dataframe.drop(['mzn', 'dzn'], axis=1)
+    if target['preprocessing']['drop_constant_values']:
+        result.drop(result.columns[result.nunique() == 1], axis=1, inplace=True)  # drop cols with constant value
+
+    if target['preprocessing']['scale']:
+        transformer = MaxAbsScaler().fit(result)
+        result = pd.DataFrame(transformer.transform(result), columns=result.columns,
+                              index=result.index)  # normalise data
+    return result
 
 
 def cross_validate(target, features_at_percent):
-    
+    percentage = target['percentage']
+    data_at_percentage: pd.DataFrame = features_at_percent[percentage]
+
+    if not target['use_ewma']:
+        data_at_percentage = data_at_percentage[
+            data_at_percentage.columns[~data_at_percentage.columns.str.contains("ewma")]
+        ]
+
+    if not target['use_gradient']:
+        data_at_percentage = data_at_percentage[
+            data_at_percentage.columns[~data_at_percentage.columns.str.contains("gradient")]
+        ]
+
+    model = create_model(target['model'])
+
+    data_at_percentage = preprocessing(data_at_percentage, target)
+
+    # K-fold
+    stratified_k_fold = StratifiedKFold(n_splits=target['k_fold']['n_splits'], shuffle=True)
+
+    f1_scores = []
+
+    for train, test in stratified_k_fold.split(
+            data_at_percentage.drop(columns=["solved_within_time_limit"]),
+            data_at_percentage["solved_within_time_limit"]):
+        train_data = data_at_percentage.iloc[train]
+        test_data = data_at_percentage.iloc[test]
+
+        train_x, train_y = train_data.drop(columns=["solved_within_time_limit"]), train_data["solved_within_time_limit"]
+        test_x, test_y = test_data.drop(columns=["solved_within_time_limit"]), test_data["solved_within_time_limit"]
+        model = model.fit(train_x, train_y)
+
+        predictions = model.predict(test_x)
+
+        current_f1_score = f1_score(test_y, predictions)
+        f1_scores.append(current_f1_score)
+
+    return {
+        'f1_scores': f1_scores,
+        'std': np.std(f1_scores),
+        'maximum': max(f1_scores),
+        'average': np.average(f1_scores)
+    }
 
 
 def main():
@@ -57,8 +116,7 @@ def main():
     )
     parser.add_argument(
         "--output",
-        type=int,
-        default=1,
+        type=str,
         help="Path to output directory",
     )
 
@@ -69,7 +127,7 @@ def main():
     output_path = Path(args.output)
 
     output_path.mkdir(parents=True, exist_ok=True)
-    logger.debug(f"Created output directory {output_dir}")
+    logger.debug(f"Created output directory {output_path}")
 
     # Load the problem output json files into a dataframe
     target = json.loads(target_path.read_text())
